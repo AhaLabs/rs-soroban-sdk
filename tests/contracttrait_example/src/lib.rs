ad5c1c65abc269039f45d8439779d0d41e046e33
Willem Wyndham <willem@ahalabs.dev>
2025-07-03 22:21:53 -0400

    feat: add contracttrait and derive_contract macros

---
+++ tests/contracttrait_example/src/lib.rs
@@ -0,0 +1,19 @@
+#![no_std]
+
+use contracttrait_lib::{Administratable, AdministratableExt, Upgradable};
+
+use soroban_sdk::{contract, derive_contract, Env};
+
+#[contract]
+#[derive_contract(
+    Administratable, 
+    Upgradable(ext = AdministratableExt)
+)]
+pub struct Contract;
+
+#[soroban_sdk::contractimpl]
+impl Contract {
+    pub fn __constructor(env: &Env, admin: soroban_sdk::Address) {
+        Self::init(env, &admin);
+    }
+}
